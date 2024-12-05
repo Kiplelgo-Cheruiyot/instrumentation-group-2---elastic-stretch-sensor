2c3a2721c4894588f7a0c6dafad762c485eda15a
Bernie Kiplelgo Cheruiyot <cheruiyot.bernie@gmail.com>
2024-12-05 10:06:06 +0100

    - Removed initialLength averaging function, updated Serial Monitor

--- main.ts
+++ main.ts
@@ -5,21 +5,20 @@ let avgInitLength = 0
 let initLength = 0
 let initResistance = 0
 let voltageInit = 0
-basic.pause(5000)
+basic.pause(2000)
 for (let index = 0; index <= 9; index++) {
     voltageInit = pins.analogReadPin(AnalogPin.P0)
     initResistance = 10000 * voltageInit / (1023 - voltageInit)
     initLength = initResistance / 150
-    avgInitLength = avgInitLength + initLength / (index + 1)
-    basic.pause(100)
 }
-basic.pause(1000)
-serial.writeValue("Inital Length", avgInitLength)
+basic.pause(500)
+serial.writeValue("Inital Length", initLength)
 let thresholdVal = avgInitLength * 1.25
 basic.forever(function () {
     voltageCurrent = pins.analogReadPin(AnalogPin.P0)
     currentResistance = 10000 * voltageCurrent / (1023 - voltageCurrent)
     currentLength = currentResistance / 150
+    serial.writeValue("current Length", currentLength)
     if (currentLength >= thresholdVal) {
         basic.showIcon(IconNames.Sad)
     } else {
